422c00ba83fabf434d012bd13cb0b569a0e666a7
Satyam8009 <83566911+Satyam8009@users.noreply.github.com>
2022-07-09 10:33:06 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use('/static', express.static('static'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "\\index.html");
+})
+
+app.listen(3000, (req, res) => {
+    console.log("Server started running on port 3000");
+})
